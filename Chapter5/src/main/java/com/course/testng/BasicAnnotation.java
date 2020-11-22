3a3c3bc8ef0c753555d9ad78a6c7244cff4d14f4
liuyang <liuyang@zhiketong.cn>
2020-11-22 20:06:17 +0800

    增加beforeMethod/afterMethod/beforeClass/afterClass

--- Chapter5/src/main/java/com/course/testng/BasicAnnotation.java
+++ Chapter5/src/main/java/com/course/testng/BasicAnnotation.java
@@ -1,10 +1,35 @@
 package com.course.testng;
 
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class BasicAnnotation {
+    @BeforeClass
+    public void beforeClass() {
+        System.out.println("beforeClass");
+    }
+
     @Test
-    public void testCase1(){
-        System.out.println("第一个测试用例!");
+    public void testCase1() {
+        System.out.print("第一个测试用例!");
+    }
+
+    @Test
+    public void testCase2() {
+        System.out.print("第二个测试用例！");
+    }
+
+    @BeforeMethod
+    public void beforeMethod() {
+        System.out.print("这是beforeMethod方法");
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+        System.out.println("这是afterMethod方法");
+    }
+
+    @AfterClass
+    public void afterClass() {
+        System.out.println("afterClass");
     }
 }
