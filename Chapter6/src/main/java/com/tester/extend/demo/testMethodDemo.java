c4442df8c4353ba42940e54fe76ce1511ea74a4d
liuyang <liuyang@zhiketong.cn>
2020-12-02 09:47:34 +0800

    testng ExtentReport、mock服务器

---
+++ Chapter6/src/main/java/com/tester/extend/demo/testMethodDemo.java
@@ -0,0 +1,31 @@
+package com.extend.demo;
+
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class testMethodDemo {
+    @Test
+    public void test1(){
+        System.out.println("这是test1（）");
+        Assert.assertEquals(1,1);
+
+    }
+    @Test
+    public void test2(){
+        System.out.println("这是test2（）");
+        Assert.assertEquals("1","2");
+    }
+
+    @Test
+    public void test3(){
+        System.out.println("这是test3（）");
+        Assert.assertEquals("fdf","fdf");
+
+    }
+    @Test
+    public void logDemo() throws RuntimeException {
+        Reporter.log("这是我自己写的日志");
+        throw new RuntimeException("这是人为造的一个RuntimeException异常");
+    }
+}
